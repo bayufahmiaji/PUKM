c1dcb2ccbab8f905039a04f70248ebd76a58426c
ndikandik020@gmail.com <ndikandik020@gmail.com>
2019-01-07 14:44:22 +0700

    project update

--- PUKM/src/pukm/PUKM.java
+++ PUKM/src/pukm/PUKM.java
@@ -15,7 +15,7 @@ public class PUKM {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here by andika
+        // TODO code application logic here by andika ganteng
     }
     
 }
